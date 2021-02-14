af4b68bda58a95738ea03581ce5246467ad44bd8
giovannapezzini <giovannarpezzini@gmail.com>
2021-02-13 23:31:40 -0300

    Update value of currentFilter

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -188,7 +188,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
         
