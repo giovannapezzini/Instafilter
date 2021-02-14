ce895f1c0b013682eeab0cd35f4f76a15a5f0ed6
giovannapezzini <giovannarpezzini@gmail.com>
2021-02-13 22:42:14 -0300

    Unwrap outputImage

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -166,7 +166,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
         
-        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             self.imageView.image = processedImage
         }
